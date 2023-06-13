7672691a0f4499665079537e34e4f8f11b2337a4
Kartik Sharma <kartik4949@gmail.com>
2023-06-14 00:25:54 +0530

    ref(context): add log disposer into logging context

--- aijson/program.py
+++ aijson/program.py
@@ -2,8 +2,36 @@ import contextlib
 from aijson.decorate import aijson
 
 
+class JsonDisposer:
+    def __init__(self, path):
+        self.path = path
+
+    def dump(self, data):
+        with open(self.path, "w") as f:
+            json.dump(data, f)
+
+
+def formatter_factory(format="json", path=None):
+    if format == "json":
+        return JsonDisposer(path=path)
+    else:
+        raise NotImplementedError(f"{format} not supported yet!")
+
+
+class LogDisposer:
+    def __init__(self, data=None):
+        self.data = data
+
+    def dump(path=".model.ai.json", format="json"):
+        formatter = formatter_factory(format=format, path=path)
+        formatter.dump(self.data)
+
+    def __str__(self):
+        return json.dumps(self.data)
+
+
 @contextlib.contextmanager
 def logging_context():
     aijson.reset()
-    yield aijson.graph
+    yield LogDisposer(data=aijson.graph)
     aijson.reset()
